d27fda4d590c4e2c805016d4a86d7f914edd5f66
Mashtruman <Mashtruman07@gmail.com>
2022-07-03 13:06:00 -0700

    4. Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 	/**
 	 * main - Entry point
 	 *
+	 * Description - use printf
 	 * Return: Always 0 (Success)
 	 */
 	int main(void)
 	{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf(%"%s", "with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 	}
