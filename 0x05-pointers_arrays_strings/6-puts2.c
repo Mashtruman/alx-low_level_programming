49c5fbed6138710be1747cd796a5eac16258c499
Mashtruman <Mashtruman07@gmail.com>
2022-07-11 15:39:23 -0700

    starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /** 
  * puts2 -  a function that prints every other character of a string,
-             starting with the first character, followed by a new line.
+ * starting with the first character, followed by a new line.
  * @str: input
  * Return: print
  */
