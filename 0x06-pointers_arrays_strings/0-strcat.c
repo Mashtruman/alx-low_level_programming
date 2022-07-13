963b21fcc711157e21e008e91d4bd7c06b62b8b4
Mashtruman <Mashtruman07@gmail.com>
2022-07-13 13:42:53 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strcat - appends src to the dest string
- *  @dest: string to append by src
- *  @src: string to append to dest
+ * @dest: string to append by src
+ * @src: string to append to dest
  *
- *   Return: address of dest
+ *  Return: address of dest
  */
 
 char *_strcat(char *dest, char *src)
