c1fb4035ed12acb140cdf632861e7d4b429eacf2
Mashtruman <Mashtruman07@gmail.com>
2022-07-13 14:10:48 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  _strcmp - compares two strings
+ * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
  *
@@ -14,5 +14,5 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 		i++;
-
+	return (*(s1 + i) - *(s2 + i));
 }
